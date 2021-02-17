35c0e2c6cc48be1018b19b38ee2996b92a0c8503
Reinis Danne <rei4dan@gmail.com>
2021-02-17 13:14:50 +0200

    03-array-objects.php: Improve variable naming
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- prep/03-array-objects.php
+++ prep/03-array-objects.php
@@ -2,9 +2,9 @@
 
 echo "===== Exercise 1\n";
 // Create a non-associative array with 3 integer values and display the total sum of them.
-$arr = [23, 43, 56,];
+$numbers = [23, 43, 56,];
 
-echo array_sum($arr) . "\n";
+echo array_sum($numbers) . "\n";
 
 echo "\n===== Exercise 2\n";
 // Using dump method, dump out all 3 values of the given array.
@@ -27,7 +27,7 @@ var_dump($person);
 
 echo "\n===== Exercise 4\n";
 // Program should display concatenated value of - Jane Doe 41
-$items = [
+$people = [
     [
         [
             "name" => "John",
@@ -42,9 +42,9 @@ $items = [
     ]
 ];
 
-echo "{$items[0][1]['name']} {$items[0][1]['surname']} {$items[0][1]['age']}\n";
+echo "{$people[0][1]['name']} {$people[0][1]['surname']} {$people[0][1]['age']}\n";
 
 echo "\n===== Exercise 5\n";
 // Program should display concatenated value of - John & Jane Doe`s
 
-echo "{$items[0][0]['name']} & {$items[0][1]['name']} {$items[0][1]['surname']}'s\n";
+echo "{$people[0][0]['name']} & {$people[0][1]['name']} {$people[0][1]['surname']}'s\n";
