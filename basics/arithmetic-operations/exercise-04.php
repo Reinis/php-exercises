6541b74ac0b4fd25ec6c5508ccb9c8a0351939ca
Reinis Danne <rei4dan@gmail.com>
2021-02-17 18:17:25 +0200

    Add arithmetic-operations/exercise-04.php
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

---
+++ basics/arithmetic-operations/exercise-04.php
@@ -0,0 +1,16 @@
+<?php
+
+// Write a program to compute the product of integers from 1 to 10
+// (i.e., 1×2×3×...×10), as an int. Take note that it is the same as
+// factorial of N.
+$start = 1;
+$end = 10;
+
+$product = 1;
+
+while ($start <= $end) {
+    $product *= $start;
+    $start++;
+};
+
+echo "!{$end} = " . $product . PHP_EOL;
