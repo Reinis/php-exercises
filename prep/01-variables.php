f3cdedfe310f5419f8d5698ca38237c291678a02
Reinis Danne <rei4dan@gmail.com>
2021-02-17 13:06:08 +0200

    01-variables.php: Improve variable naming
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- prep/01-variables.php
+++ prep/01-variables.php
@@ -2,24 +2,24 @@
 
 // Create variable that prints out an integer 10, float 10.10, string "hello world"
 echo "===== Exercise 1\n";
-$i = 10;
-$f = 10.10;
-$s = "hello world";
+$wholeNumber = 10;
+$decimalNumber = 10.10;
+$text = "hello world";
 
-echo "{$i}\n";
-echo number_format($f, 2, '.', '') . "\n";
-echo "{$s}\n";
+echo "{$wholeNumber}\n";
+echo number_format($decimalNumber, 2, '.', '') . PHP_EOL;
+echo "{$text}\n";
 
 // Dump the same values that should display both data type & its value.
 // (Note, usage of var_dump)
 echo "\n===== Exercise 2\n";
-var_dump($i, $f, $s);
+var_dump($wholeNumber, $decimalNumber, $text);
 
 // Concatenate your name, surname and integer of your age.
 echo "\n===== Exercise 3\n";
-$t = new stdClass();
-$t->name = 'Reinis';
-$t->surname = 'Danne';
-$t->age = 360;
+$person = new stdClass();
+$person->name = 'Reinis';
+$person->surname = 'Danne';
+$person->age = 360;
 
-echo "{$t->name} {$t->surname} {$t->age}\n";
+echo "{$person->name} {$person->surname} {$person->age}\n";
