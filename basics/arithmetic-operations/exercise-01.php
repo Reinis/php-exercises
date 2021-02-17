9eecd1cdd0c599a3f073cb020df4ba2a86caa8a8
Reinis Danne <rei4dan@gmail.com>
2021-02-17 15:05:38 +0200

    exercise-01.php: Use readline()
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- basics/arithmetic-operations/exercise-01.php
+++ basics/arithmetic-operations/exercise-01.php
@@ -2,14 +2,8 @@
 
 // Write a program to accept two integers and return true if the either one
 // is 15 or if their sum or difference is 15.
-$usage = 'usage: php exercise-01.php number1 number2' . PHP_EOL;
-
-if (!empty($argv[1]) and !empty($argv[2])) {
-    $x = $argv[1];
-    $y = $argv[2];
-} else {
-    die($usage);
-}
+$x = readline('-> First integer: ');
+$y = readline('-> Second integer: ');
 
 if ($x == 15 or $y == 15 or abs($x - $y) === 15) {
     echo 'true';
