9619dac90964d5aaff59c2bdfd464645fb5e015e
Reinis Danne <rei4dan@gmail.com>
2021-02-17 18:31:03 +0200

    Add arithmetic-operations/exercise-05.php
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

---
+++ basics/arithmetic-operations/exercise-05.php
@@ -0,0 +1,34 @@
+<?php
+
+// Write a program that picks a random number from 1-100. Give the user
+// a chance to guess it. If they get it right, tell them so. If their guess
+// is higher than the number, say "Too high." If their guess is lower than
+// the number, say "Too low." Then quit. (They don't get any more guesses
+// for now.)
+//
+// I'm thinking of a number between 1-100.  Try to guess it.
+// > 13
+//
+// Sorry, you are too low.  I was thinking of 34.
+//
+// I'm thinking of a number between 1-100.  Try to guess it.
+// > 79
+//
+// Sorry, you are too high.  I was thinking of 51.
+//
+// I'm thinking of a number between 1-100.  Try to guess it.
+// > 42
+//
+// You guessed it!  What are the odds?!?
+$number = random_int(1, 100);
+
+echo "I'm thinking of a number in range 1–100. Try to guess it." . PHP_EOL;
+$guess = readline('> ');
+
+if ($number > $guess) {
+    echo 'Sorry, you are too low. I was thinking ' . $number . PHP_EOL;
+} elseif ($number < $guess) {
+    echo 'Sorry, you are too high. I was thinking ' . $number . PHP_EOL;
+} else {
+    echo 'You guessed it! What are the odds?!?' . PHP_EOL;
+}
