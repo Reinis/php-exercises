a8dc96ff333038815be49fe8adfa7d2359c63c2a
Reinis Danne <rei4dan@gmail.com>
2021-02-21 15:25:52 +0200

    arithmetic-operations/exercise-02.php: Add a loop
    Keep asking for an integer unless 'q' is provided. Validate the input to
    check that it was an integer.
    
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- basics/arithmetic-operations/exercise-02.php
+++ basics/arithmetic-operations/exercise-02.php
@@ -3,8 +3,20 @@
 // Write a program called CheckOddEven which prints "Odd Number" if the
 // int variable “number” is odd, or “Even Number” otherwise. The program shall
 // always print “bye!” before exiting.
-$number = readline('-> Integer: ');
+while (true) {
+    do {
+        $number = readline('-> Integer (q to quit): ');
 
-echo ($number % 2 === 0 ? 'Even' : 'Odd') . ' Number' .  PHP_EOL;
+        if ($number === 'q') {
+            echo 'bye!' . PHP_EOL;
+            exit(0);
+        }
 
-echo 'bye!' . PHP_EOL;
+        $number = filter_var($number, FILTER_VALIDATE_INT);
+    } while ($number === false);
+
+    $number = (int)$number;
+
+    echo ($number % 2 === 0 ? 'Even' : 'Odd') . ' Number' . PHP_EOL;
+}
+
