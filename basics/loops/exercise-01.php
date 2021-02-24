996a7aa23b7b29297b3bb2b9c8ba226f14a67a8d
Reinis Danne <rei4dan@gmail.com>
2021-02-24 19:28:18 +0200

    Add loops/exercise-01.php
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

---
+++ basics/loops/exercise-01.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "The first 10 natural numbers are:";
+
+//todo write a program that displays first 10 natural numbers
+for ($i = 0; $i < 10; $i++) {
+    echo " $i";
+}
+
+echo PHP_EOL;
