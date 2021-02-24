c6ab357387e5703252fdcc0e90d9884ed551c7be
Reinis Danne <rei4dan@gmail.com>
2021-02-24 19:59:03 +0200

    loops/exercise-02.php: Ask for base and power
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- basics/loops/exercise-02.php
+++ basics/loops/exercise-02.php
@@ -1,14 +1,18 @@
 <?php
 
 do {
-    $number = filter_var(readline("Input number of terms: "), FILTER_VALIDATE_INT);
-} while ($number === false);
+    $base = filter_var(readline("-> Integer: "), FILTER_VALIDATE_INT);
+} while ($base === false);
+
+do {
+    $power = filter_var(readline("-> Number of terms: "), FILTER_VALIDATE_INT);
+} while ($power === false);
 
 //todo complete loop to multiply i with itself n times, it is NOT allowed to use built-in pow() function
 $product = 1;
 
-for ($i = 0; $i < $number; $i++) {
-    $product *= $number;
+for ($i = 0; $i < $power; $i++) {
+    $product *= $base;
 }
 
-echo "Product = {$number}^{$number} = {$product}\n";
+echo "Product = {$base}^{$power} = {$product}\n";
