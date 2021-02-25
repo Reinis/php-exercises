1ea8f020c9503db67fd0b1c2432bbdc74c34426d
Reinis Danne <rei4dan@gmail.com>
2021-02-25 22:01:22 +0200

    flow-of-control/exercise-05.php: Use switch
    Use a `switch` statement as per exercise requirements.
    Also, use a nested `if` statement to exit the program loop.
    
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- basics/flow-of-control/exercise-05.php
+++ basics/flow-of-control/exercise-05.php
@@ -1,62 +1,116 @@
 <?php
 
+/**
+ * On your phone keypad, the alphabets are mapped to digits as follows:
+ * ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), WXYZ(9).
+ *
+ * Write a program called PhoneKeyPad, which prompts user for a String (case
+ * insensitive), and converts to a sequence of keypad digits.
+ *
+ * Use:
+ *
+ *   a "nested-if" statement
+ *   a "switch-case-default" statement
+ *
+ * Hint: In switch-case, you can handle multiple cases by omitting the break
+ * statement, e.g.,
+ */
+
 declare(strict_types=1);
+
 
 namespace PhoneKeyPad;
 
-// On your phone keypad, the alphabets are mapped to digits as follows:
-// ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), WXYZ(9).
-//
-// Write a program called PhoneKeyPad, which prompts user for a String (case
-// insensitive), and converts to a sequence of keypad digits.
-//
-// Use:
-//
-//    a "nested-if" statement
-//    a "switch-case-default" statement
-//
-// Hint: In switch-case, you can handle multiple cases by omitting the break
-// statement, e.g.,
 
 class PhoneKeyPad
 {
-    private array $keyMap = [
-        2 => 'ABC',
-        3 => 'DEF',
-        4 => 'GHI',
-        5 => 'JKL',
-        6 => 'MNO',
-        7 => 'PQRS',
-        8 => 'TUV',
-        9 => 'WXYZ',
-    ];
-
-    private array $letterMap = [
-        'A' => 2, 'B' => 2, 'C' => 2,
-        'D' => 3, 'E' => 3, 'F' => 3,
-        'G' => 4, 'H' => 4, 'I' => 4,
-        'J' => 5, 'K' => 5, 'L' => 5,
-        'M' => 6, 'N' => 6, 'O' => 6,
-        'P' => 7, 'Q' => 7, 'R' => 7, 'S' => 7,
-        'T' => 8, 'U' => 8, 'V' => 8,
-        'W' => 9, 'X' => 9, 'Y' => 9, 'Z' => 9,
-    ];
-
     public function getNumbers(string $input): string
     {
         $result = '';
 
-        foreach (str_split($input) as $letter) {
-            if (!isset($this->letterMap[$letter])) {
-                $result .= ' ';
-                continue;
-            }
-
-            $number = $this->letterMap[$letter];
-            $repeats = strpos($this->keyMap[$number], $letter) + 1;
-
-            for ($i = 0; $i < $repeats; $i++) {
-                $result .= $number;
+        foreach (mb_str_split($input) as $letter) {
+            switch ($letter) {
+                case 'C':
+                    $result .= '2';
+                // no break
+                case 'B':
+                    $result .= '2';
+                // no break
+                case 'A':
+                    $result .= '2';
+                    break;
+                case 'F':
+                    $result .= '3';
+                // no break
+                case 'E':
+                    $result .= '3';
+                // no break
+                case 'D':
+                    $result .= '3';
+                    break;
+                case 'I':
+                    $result .= '4';
+                // no break
+                case 'H':
+                    $result .= '4';
+                // no break
+                case 'G':
+                    $result .= '4';
+                    break;
+                case 'L':
+                    $result .= '5';
+                // no break
+                case 'K':
+                    $result .= '5';
+                // no break
+                case 'J':
+                    $result .= '5';
+                    break;
+                case 'O':
+                    $result .= '6';
+                // no break
+                case 'N':
+                    $result .= '6';
+                // no break
+                case 'M':
+                    $result .= '6';
+                    break;
+                case 'S':
+                    $result .= '7';
+                // no break
+                case 'R':
+                    $result .= '7';
+                // no break
+                case 'Q':
+                    $result .= '7';
+                // no break
+                case 'P':
+                    $result .= '7';
+                    break;
+                case 'V':
+                    $result .= '8';
+                // no break
+                case 'U':
+                    $result .= '8';
+                // no break
+                case 'T':
+                    $result .= '8';
+                    break;
+                case 'Z':
+                    $result .= '9';
+                // no break
+                case 'Y':
+                    $result .= '9';
+                // no break
+                case 'X':
+                    $result .= '9';
+                // no break
+                case 'W':
+                    $result .= '9';
+                    break;
+                default:
+                    $result .= ' ';
+                    break;
             }
         }
 
@@ -67,7 +121,19 @@ class PhoneKeyPad
 
 $program = new PhoneKeyPad();
 
-$input = strtoupper(readline('-> Enter a string: '));
+while (true) {
+    $input = strtoupper(readline('-> Enter a string: '));
 
-echo $program->getNumbers($input);
-echo PHP_EOL;
+    if (ctype_digit($input)) {
+        $answer = strtoupper(readline('-> Quit? [Y/n] '));
+
+        if ($answer === '' || $answer === 'Y') {
+            break;
+        }
+
+        continue;
+    }
+
+    echo $program->getNumbers($input);
+    echo PHP_EOL;
+}
