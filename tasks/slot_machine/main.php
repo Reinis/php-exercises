34ac7f88d80339f20d5341261c9b2c10c09959c8
Reinis Danne <rei4dan@gmail.com>
2021-02-28 00:09:36 +0200

    slot_machine: Simplify main logic
    Get rid of the nested while loop for the bonus games and use the same
    loop for both.
    
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tasks/slot_machine/main.php
+++ tasks/slot_machine/main.php
@@ -48,7 +48,6 @@ const ENABLE_CURSOR = "\e[?25h";
 const GO_TO_LINE_START = "\r";
 const GO_ONE_LINE_UP = "\e[1A";
 const GO_FOUR_LINES_UP = "\e[4A";
-const GO_FIVE_LINES_UP = "\e[5A";
 const CLEAR_LINE = "\e[2K";
 
 
@@ -64,32 +63,37 @@ do {
 $game->setAmount($amount);
 initRolls($game);
 
-while ($game->getAmount() >= 10) {
-    do {
-        echo CLEAR_LINE . GO_TO_LINE_START;
-        $bet = filter_var(
-            readline("-> Bet: "),
-            FILTER_VALIDATE_INT
-        );
-    } while ($bet === false || $bet < 10 || $bet % 10 !== 0);
+while ($game->getAmount() >= 10 || $game->getBonus() !== 0) {
+    if ($game->getBonus() === 0) {
+        do {
+            echo CLEAR_LINE . GO_TO_LINE_START;
 
-    // Check available money
-    if ($bet > $game->getAmount()) {
-        echo GO_ONE_LINE_UP . CLEAR_LINE;
-        echo "You don't have enough money for the bet!";
-        sleep(2);
-        continue;
+            $bet = filter_var(
+                readline("-> Bet: "),
+                FILTER_VALIDATE_INT
+            );
+        } while ($bet === false || $bet < 10 || $bet % 10 !== 0);
+
+        echo GO_ONE_LINE_UP;
+
+        // Check available money
+        if ($bet > $game->getAmount()) {
+            echo CLEAR_LINE;
+            echo "You don't have enough money for the bet!";
+            sleep(2);
+            continue;
+        }
+
+        $game->setBet($bet);
     }
 
-    $game->setBet($bet);
-
-    echo GO_TO_LINE_START . GO_FIVE_LINES_UP . DISABLE_CURSOR;
+    echo GO_TO_LINE_START . GO_FOUR_LINES_UP . DISABLE_CURSOR;
 
     initRolls($game);
     echo GO_TO_LINE_START . GO_FOUR_LINES_UP;
     sleep(1);
 
-    $game->play();
+    $game->play($game->getBonus() > 0);
 
     displayRolls($game);
 
@@ -99,29 +103,6 @@ while ($game->getAmount() >= 10) {
         $game->getBonus(),
         $game->getAmount()
     );
-
-    // Check for bonus games
-    while ($game->getBonus() > 0) {
-        // Autoplay bonus games
-        for ($i = 0; $i < Game::NUMBER_OF_BONUS_GAMES; $i++) {
-            echo GO_TO_LINE_START . GO_FOUR_LINES_UP;
-            initRolls($game);
-            echo GO_TO_LINE_START . GO_FOUR_LINES_UP;
-            sleep(1);
-
-            $game->play(true);
-
-            displayRolls($game);
-
-            echo CLEAR_LINE;
-            printf(
-                "Prize: %4d bonus: %4d available: %4d\n",
-                $game->getPrize(),
-                $game->getBonus(),
-                $game->getAmount()
-            );
-        }
-    }
 
     echo ENABLE_CURSOR;
 }
