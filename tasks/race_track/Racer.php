15df6be5c0a9baa931ba0d28b9fb173b92b8316a
Reinis Danne <rei4dan@gmail.com>
2021-03-10 22:59:41 +0200

    race_track: Remove unused Racer::getLargestSpeed()
    The constructor of the Racers collection accepts Movables, so the
    largest speed can be passed through directly from those.
    
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tasks/race_track/Racer.php
+++ tasks/race_track/Racer.php
@@ -7,13 +7,11 @@ class Racer
     private Movable $racer;
     private int $progress = 0;
     private int $time = 0;
-    private int $largestSpeed;
     private bool $crashed = false;
 
     public function __construct(Movable $racer)
     {
         $this->racer = $racer;
-        $this->largestSpeed = $racer->getLargestSpeed();
     }
 
     public function getName(): string
@@ -46,11 +44,6 @@ class Racer
     public function getTime(): int
     {
         return $this->time;
-    }
-
-    public function getLargestSpeed(): int
-    {
-        return $this->largestSpeed;
     }
 
     public function isCrashed(): bool
