1320c322f9a63f8b6faa4f27d04fb26f66ff98cb
Reinis Danne <rei4dan@gmail.com>
2021-03-20 21:17:35 +0200

    flower_shop_web: Fetch data as anonymous objects
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tasks/flower_shop_web/app/Warehouse1.php
+++ tasks/flower_shop_web/app/Warehouse1.php
@@ -12,16 +12,15 @@ class Warehouse1 extends Warehouse
 
         $options = [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
 
         $connection = new PDO($connectionString, $user, $password, $options);
         $data = $connection->query("select * from `Warehouse1`");
-        $connection = null;
 
-        foreach ($data as ['name' => $flower, 'amount' => $amount]) {
-            $flowers->addFlower(new Flower($flower, $amount));
+        foreach ($data as $item) {
+            $flowers->addFlower(new Flower($item->name, $item->amount));
         }
 
         parent::__construct($name, $flowers);
