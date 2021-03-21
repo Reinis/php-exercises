508522dd91cb20d332771609ac1c02c5bf4d92e6
Reinis Danne <rei4dan@gmail.com>
2021-03-21 11:47:32 +0200

    flower_shop_web: Avoid possible credential leak
    Backtraces can contain connection credentials. Rethrow the exception to
    avoid that possibility.
    
    In production the server should be configured to log errors, avoiding
    displaying the message to the user even if there is sensitive
    information in the trace.
    
    Signed-off-by: Reinis Danne <rei4dan@gmail.com>

--- tasks/flower_shop_web/app/Warehouse1.php
+++ tasks/flower_shop_web/app/Warehouse1.php
@@ -3,6 +3,7 @@
 namespace FlowerShopWeb;
 
 use PDO;
+use PDOException;
 
 class Warehouse1 extends Warehouse
 {
@@ -16,7 +17,12 @@ class Warehouse1 extends Warehouse
             PDO::ATTR_EMULATE_PREPARES => false,
         ];
 
-        $connection = new PDO($connectionString, $user, $password, $options);
+        try {
+            $connection = new PDO($connectionString, $user, $password, $options);
+        } catch (PDOException $e) {
+            throw new PDOException($e->getMessage(), (int)$e->getCode());
+        }
+
         $data = $connection->query("select * from `Warehouse1`");
 
         foreach ($data as $item) {
